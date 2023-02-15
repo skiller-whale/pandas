dfd8fd2639d2d84988b61abb55a97cb1ee7fd270
Traiko Dinev <tdinev@skillerwhale.com>
2023-02-15 15:32:53 +0000

    Add exercises for indexing

---
+++ indexing/exercise_3.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from utils.dataset_loader import load_whale_dataset
+
+# pylint: disable=pointless-string-statement
+"""
+EXERCISE 3: Chained Indexing
+------------------------
+
+This exercise has the same queries as the previous exercises.
+    Write the same queries using chained indexing,
+    combining `[]`, `loc`, or `iloc.
+
+    * Select the age and whale_type of the first 100 whales.
+    * Select the weight of whale number 567
+    * Select all columns, EXCEPT the whale_type of the LAST 200 whales
+
+    * Uncomment the assert statements to check your answers.
+
+Once you've finished the exercise,
+    think about the order of indexing (no need to rewrite anything).
+    * Discuss this with your coach.
+"""
+
+
+whale_df = load_whale_dataset()
+# YOUR CODE GOES HERE
+
+# * Select the age and whale_type of the first 100 whales.
+age_whale_type_first_100 = ...
+
+# * Select the weight of whale number 567
+whale_567_weight = ...
+
+# * Select all columns, EXCEPT the gender of the LAST 200 whales
+last_200_whales_except_gender = ...
+
+# # <<< DO NOT CHANGE THE LINES BELOW HERE (except to uncomment) >>>
+# assert age_whale_type_first_100.shape == (100, 2), "Shape of age_whale_type_first_100 is wrong!"
+# assert np.isclose(whale_567_weight, 6.641571451865368, rtol=1e-6), "Weight of whale 567 is wrong!"
+# assert last_200_whales_except_gender.shape == (200, 4), "Shape of last_200_whales_except_gender is wrong!"
---
+++ indexing/utils/dataset_loader.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from pathlib import Path
+
+DATA_PATH = Path(__file__).parent.parent / 'data'
+
+
+def load_fishguard_dataset():
+    """Loads the Fishguard temperatures dataset.
+    Returns: A pandas Series containing average temperatures in Fishguard over months.
+    """
+    df = pd.read_csv(DATA_PATH / 'fishguard_mean_temps.csv', index_col=0)
+    
+    # return a series of the (only) column in this dataset
+    return df.iloc[:, 0]
+
+
+def load_whale_dataset():
+    """Loads the (fake) clean whale dataset.
+    Returns: A pandas DataFrame containing the whale dataset,
+    """
+    return pd.read_csv(DATA_PATH / 'fake_whale_dataset_clean.csv', index_col=0)
