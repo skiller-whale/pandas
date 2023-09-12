cbb1430c85fef688b7a96bd4b7063fb4ed7bed72
Traiko Dinev <tdinev@skillerwhale.com>
2023-09-12 12:03:28 +0100

    Rework exercises to use movies dataset

--- exercises/indexing/utils/dataset_loader.py
+++ exercises/indexing/utils/dataset_loader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
 
-DATA_PATH = Path(__file__).parent.parent / 'data'
+DATA_PATH = Path(__file__).parent.parent.parent / 'data'
 
 
 def load_fishguard_dataset():
@@ -14,8 +14,13 @@ def load_fishguard_dataset():
     return df.iloc[:, 0]
 
 
-def load_whale_dataset():
-    """Loads the (fake) clean whale dataset.
-    Returns: A pandas DataFrame containing the whale dataset,
+def load_movies_dataset():
+    """Loads the movies dataset.
+    Returns: A pandas DataFrame containing the movies dataset,
     """
-    return pd.read_csv(DATA_PATH / 'fake_whale_dataset_clean.csv', index_col=0)
+    df = pd.read_csv(DATA_PATH / 'movies' / 'movies_small.csv', index_col=0)
+    
+    # add one to the indexing so it starts from 2
+    df.index += 2
+    
+    return df
